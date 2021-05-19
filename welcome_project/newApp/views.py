251ed0ebd4015eea125bdb07fa03822f6184d32d
함승우 <hamseung-u@hamseung-uui-MacBookAir.local>
2021-05-19 21:38:36 +0900

    add Welcome.html and hello.html

--- welcome_project/newApp/views.py
+++ welcome_project/newApp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def hello(request) :
+    userName = request.GET['name']
+    return render(request, 'hello.html', {'userName' : userName})   
+
+def welcome(request) :
+    return render(request, 'welcome.html')
+
 def template(request) :
     return render(request, 'template.html')
