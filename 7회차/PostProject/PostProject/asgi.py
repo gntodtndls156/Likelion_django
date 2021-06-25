ceae3f929fe54b511c880357080521f4823564ce
gntodtndls156 <gntodtndls156@gmail.com>
2021-06-25 20:05:27 +0900

    model 필드 생성

--- 7회차/PostProject/PostProject/asgi.py
+++ 7회차/PostProject/PostProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for PostProject project.
+ASGI config for postproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PostProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'postproject.settings')
 
 application = get_asgi_application()
--- 7회차/PostProject/PostProject/wsgi.py
+++ 7회차/PostProject/PostProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PostProject project.
+WSGI config for postproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PostProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'postproject.settings')
 
 application = get_wsgi_application()
