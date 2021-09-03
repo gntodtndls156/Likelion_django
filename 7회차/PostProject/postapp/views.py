854f90e4168ab8713afb7f0513b055ef8a43f0c0
gntodtndls156 <gntodtndls156@gmail.com>
2021-09-03 18:25:07 +0900

    1학기 폴더

--- 7회차/PostProject/postapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-# Create your views here.
-def Home(request):
-    posts = Post.objects.all()
-    return render(request, 'Home.html', {'posts': posts})
